d2027f44b5d7b7c743dc5263a10be392a3856fb6
Guilherme Lopes <guilhermelopes97@outlook.com>
2023-10-18 18:18:45 -0300

    upload API de videos com node.js

---
+++ server.js
@@ -0,0 +1,91 @@
+import { fastify } from 'fastify'
+// import { DatabaseMemory } from './database-memory.js'
+import { DatabasePostgres } from './database-postgres.js'
+
+const server = fastify()
+
+// const database = new DatabaseMemory()
+const database = new DatabasePostgres()
+
+server.get('/', () => {
+
+    return [
+        {
+            "rota": "/videos",
+            "metodo": "POST",
+            "parametros": "title,description,duration",
+            "descricao": "insere um novo video no banco de dados"
+        },
+        {
+            "rota": "/videos",
+            "metodo": "GET",
+            "parametros": null,
+            "descricao": "lista todos os videos existentes"
+        },
+        {
+            "rota": "/videos?search=node",
+            "metodo": "GET",
+            "parametros": null,
+            "descricao": "lista todos os videos existentes com a palavre 'node'"
+        },
+        {
+            "rota": "/videos/id",
+            "metodo": "PUT",
+            "parametros": "title,description,duration",
+            "descricao": "edita todas as informações do video atravez do id"
+        },
+        {
+            "rota": "/videos/id",
+            "metodo": "DELETE",
+            "parametros": null,
+            "descricao": "delete um video do banco de dados"
+        }
+    ]
+})
+
+
+server.post('/videos', async (request, reply) => {
+    const {title, description, duration} = request.body
+
+    await database.create({
+        title,
+        description,
+        duration
+    })
+
+    return reply.status(201).send()
+})
+
+server.get('/videos', async (request) => {
+    const search = request.query.search
+
+    const videos = await database.list(search)
+
+    return videos
+})
+
+server.put('/videos/:id', async (request, reply) => {
+    const videoId = request.params.id
+    const {title, description, duration} = request.body
+
+    await database.update(videoId, {
+        title,
+        description,
+        duration
+    })
+
+    return reply.status(204).send()
+})
+
+server.delete('/videos/:id', async (request, reply) => {
+    const videoId = request.params.id
+
+    await database.delete(videoId)
+
+    return reply.status(204).send()
+})
+
+
+server.listen({
+    port: process.env.PORT ?? 3333
+})
